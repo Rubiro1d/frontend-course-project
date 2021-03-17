e7715c71a7fc3592e919cba733d12dceac981501
Rubiro1d <vm1432288@gmail.com>
2021-03-17 22:17:39 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
- 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+const fs = require("fs");
+const zlib = require("zlib");
+  
+let readableStream = fs.createReadStream("hello.txt", "utf8");
+  
+let writeableStream = fs.createWriteStream("hello.txt.gz");
+  
+let gzip = zlib.createGzip();
+  
+readableStream.pipe(gzip).pipe(writeableStream);
