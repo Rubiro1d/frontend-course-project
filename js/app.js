d24b2164b862b1c86f5362381a01b06006b644da
Rubiro1d <vm1432288@gmail.com>
2021-03-18 22:30:55 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+  
 const app = express();
- 
-app.use(function (request, response) {
-  express.static("C:/Users/vova_/internet-shop");
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+  
+app.post("/user", jsonParser, function (request, response) {
+    console.log(request.body);
+    if(!request.body) return response.sendStatus(400);
+     
+    response.json(request.body); // отправляем пришедший ответ обратно
 });
- 
+  
+app.get("/", function(request, response){
+      
+    response.sendFile(__dirname + "/index.html");
+});
+  
 app.listen(3000);
