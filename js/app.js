ef3408a55d4c65136d9fa3e638c4c1b35b885468
Rubiro1d <vm1432288@gmail.com>
2021-03-18 20:40:47 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-const fs = require("fs");
-const zlib = require("zlib");
-  
-let readableStream = fs.createReadStream("hello.txt", "utf8");
-  
-let writeableStream = fs.createWriteStream("hello.txt.gz");
-  
-let gzip = zlib.createGzip();
-  
-readableStream.pipe(gzip).pipe(writeableStream);
+const express = require("express");
+const app = express();
+ 
+app.use(function (request, response) {
+  express.static("C:/Users/vova_/internet-shop");
+});
+ 
+app.listen(3000);
